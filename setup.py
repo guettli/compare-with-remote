8d2b2fc8c86fdb2854a2d4baa833ba4d8d0c1d27
Thomas Guettler <tguettler@tbz-pariv.de>
2017-03-14 16:23:56 +0100

    added packages to setup.py.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import setuptools
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
@@ -41,13 +42,12 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
+
+    packages=setuptools.find_packages(),
 
     entry_points={
         'console_scripts': [
