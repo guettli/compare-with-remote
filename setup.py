4596302c15fa216ce9a3e6e1378fd4a1393b6448
Thomas Guettler <tguettler@tbz-pariv.de>
2017-03-22 18:33:13 +0100

    use of bumpversion

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='compare-with-remote',
 
-    version='0.1',
+    version='0.1.1',
 
     description=' Compare local script output with remote script output',
     long_description=long_description,
