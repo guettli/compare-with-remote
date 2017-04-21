4f1322f43468e58bae42ad1555bd62e21a244ac9
Thomas Guettler <tguettler@tbz-pariv.de>
2017-04-21 14:58:59 +0200

    fixed tests.

--- compare_with_remote/tests/test_compare_with_remote.py
+++ compare_with_remote/tests/test_compare_with_remote.py
@@ -7,15 +7,19 @@ from compare_with_remote.compare_with_remote import CompareURL, string_to_save_f
 
 
 class MyTestCase(unittest.TestCase):
-    def test_directory_url_parsing(self):
-        self.assertEqual({'user_at_host': None, 'directory': 'foo'}, CompareURL.parse_url('foo'))
-        self.assertEqual({'directory': '', 'user_at_host': 'foo'}, CompareURL.parse_url('foo:'))
-        self.assertEqual({'directory': '', 'user_at_host': 'user@foo'}, CompareURL.parse_url('user@foo:'))
-        self.assertEqual({'directory': '', 'user_at_host': 'user@foo'}, CompareURL.parse_url('user@foo'))
-        self.assertEqual({'directory': 'mydir', 'user_at_host': 'user@foo'},
+    def test_directory_url_parsing__directory(self):
+        self.assertEqual({'user_at_host': None, 'directory_or_cmd': 'foo', 'scheme': 'dir'}, CompareURL.parse_url('foo'))
+        self.assertEqual({'directory_or_cmd': '', 'user_at_host': 'foo', 'scheme': 'dir'}, CompareURL.parse_url('foo:'))
+        self.assertEqual({'directory_or_cmd': '', 'user_at_host': 'user@foo', 'scheme': 'dir'}, CompareURL.parse_url('user@foo:'))
+        self.assertEqual({'directory_or_cmd': '', 'user_at_host': 'user@foo', 'scheme': 'dir'}, CompareURL.parse_url('user@foo'))
+        self.assertEqual({'directory_or_cmd': 'mydir', 'user_at_host': 'user@foo', 'scheme': 'dir'},
                          CompareURL.parse_url('user@foo:mydir'))
-        self.assertEqual({'directory': '/mydir', 'user_at_host': 'user@foo'},
+        self.assertEqual({'directory_or_cmd': '/mydir', 'user_at_host': 'user@foo', 'scheme': 'dir'},
                          CompareURL.parse_url('user@foo:/mydir'))
+
+    def test_directory_url_parsing__command(self):
+        self.assertEqual({'user_at_host': 'user@host', 'directory_or_cmd': 'psql -c "select app, name from django_migrations order by id;"', 'scheme': 'cmd'},
+                         CompareURL.parse_url('cmd:user@host:psql -c "select app, name from django_migrations order by id;"'))
 
     def test_directory(self):
         self.assertEqual('mydir', CompareURL('user@foo:mydir').directory)
