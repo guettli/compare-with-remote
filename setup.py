b63d2d6d7ca3c41f1dcbc08a25cd4041b6a5eadf
Thomas Guettler <tguettler@tbz-pariv.de>
2017-04-24 08:24:45 +0200

    upload to pypi after successfull travis-ci.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author='Thomas Guettler',
     author_email='info.compare-with-remote@thomas-guettler.de',
 
-    license='Apache2',
+    license='Apache Software License 2.0',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -39,7 +39,8 @@ setup(
         'Intended Audience :: Developers',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache2',
+        'License :: OSI Approved :: Apache Software License',
+
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
